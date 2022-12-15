5ec756a99b9b90b3a446a1b9bf864adaafc8e319
haeraam <nooroongy@dyphi.com>
2022-12-16 08:47:34 +0900

    Fix build web canvas

--- lib/pages/home_page/components/home_carousel_slider.dart
+++ lib/pages/home_page/components/home_carousel_slider.dart
@@ -29,7 +29,7 @@ class _HomeCarouselSliderState extends State<HomeCarouselSlider> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   const Text(
-                    'My pick',
+                    'MY PICK',
                     style: TextStyle(fontSize: 17, color: primaryC, fontWeight: FontWeight.w900),
                   ),
                   Text(
@@ -47,27 +47,27 @@ class _HomeCarouselSliderState extends State<HomeCarouselSlider> {
             CarouselSlider(
               items: const [
                 HomeCrouselCard(
-                  title: 'pick1',
+                  title: 'PICK1',
                   coment: '노스 페이스 눕시\n이번 겨울은 노스페이스와!',
                   imgUrl: 'https://image.msscdn.net/images/goods_img/20220412/2482269/2482269_1_320.jpg',
                 ),
                 HomeCrouselCard(
-                  title: 'pick2',
+                  title: 'PICK2',
                   coment: '신혼집 필수템\n인테리어의 시작과 끝!',
                   imgUrl: 'https://image.msscdn.net/images/goods_img/20220727/2684105/2684105_2_320.jpg',
                 ),
                 HomeCrouselCard(
-                  title: 'pick3',
+                  title: 'PICK3',
                   coment: '명품도 귀엽게!\n미우미우 페니 로퍼',
                   imgUrl: 'https://image.msscdn.net/images/goods_img/20220729/2688346/2688346_1_320.jpg',
                 ),
                 HomeCrouselCard(
-                  title: 'pick4',
+                  title: 'PICK4',
                   coment: '겨울 패션의 마침표.\n기본 니트머플러',
                   imgUrl: 'https://image.msscdn.net/images/goods_img/20210906/2110699/2110699_7_320.jpg',
                 ),
                 HomeCrouselCard(
-                  title: 'pick5',
+                  title: 'PICK5',
                   coment: '대세는 살로몬 꾸안꾸 스타일의 완성',
                   imgUrl: 'https://image.msscdn.net/images/goods_img/20221123/2958053/2958053_1_320.jpg',
                 ),
---
+++ lib/pages/home_page/components/home_new_item_area.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:swyg/models/item_model.dart';
+import 'package:swyg/pages/home_page/components/home_title.dart';
+import 'package:swyg/widgets/item_widget.dart';
+
+class HomeNewItemArea extends StatelessWidget {
+  const HomeNewItemArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const HomeTitle(title: 'NEW 선물 아이템'),
+          const SizedBox(height: 16),
+          SizedBox(
+            height: 262,
+            child: NotificationListener<OverscrollIndicatorNotification>(
+              onNotification: (overScroll) {
+                overScroll.disallowIndicator();
+                return true;
+              },
+              child: ListView.builder(
+                primary: false,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) => Row(
+                  children: [
+                    ItemWidget(
+                      item: Item(
+                          productId: 'productId',
+                          productNm: '아이템명',
+                          productCm: 'productCm',
+                          productPrice: 0,
+                          productUrl: 'productUrl',
+                          productImg: 'productImg',
+                          categoryNm: ['애견인','3만원이내','남자친구','직장인','학생'],
+                          productCnt: Random().nextInt(100),
+                          productWcnt: 0,
+                          memberName: '제작자',
+                          productCreateDt: DateTime.now(),
+                          productUpdateDt: DateTime.now(),
+                          productBestcmt: 'productBestcmt'),
+                    ),
+                    const SizedBox(width: 10),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
