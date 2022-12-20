5c990ff430636f6e9e8d5e2cb16417fbf547d289
haeraam <nooroongy@dyphi.com>
2022-12-20 19:07:44 +0900

    Modify: floatingActionButton

--- lib/pages/home_page/components/custom_action_button.dart
+++ lib/pages/home_page/components/custom_action_button.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:swyg/pages/page.dart';
 import 'package:go_router/go_router.dart';
@@ -12,58 +14,120 @@ class CustomActionButton extends StatefulWidget {
 class _CustomActionButtonState extends State<CustomActionButton> {
   bool isOpend = false;
   bool showBtn = false;
+  double _turns = 0;
+  Color bgColor = Colors.black;
+  Color iconColor = Colors.white;
 
   @override
   Widget build(BuildContext context) {
-    return PageWrap(
-      child: Stack(
-        alignment: Alignment.bottomRight,
-        children: [
-          if (showBtn)
-            AnimatedContainer(
+    return Stack(
+      alignment: Alignment.bottomRight,
+      children: [
+        if (showBtn)
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeIn,
+            margin: isOpend ? const EdgeInsets.only(bottom: 100, right: 0) : EdgeInsets.zero,
+            child: AnimatedOpacity(
               duration: const Duration(milliseconds: 300),
-              curve: Curves.easeIn,
-              margin: isOpend ? const EdgeInsets.only(bottom: 115, right: 0) : EdgeInsets.zero,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 300),
-                opacity: isOpend ? 1 : 0,
-                child: FloatingActionButton.extended(
-                  heroTag: "addItem",
-                  label: const Text('아이템 추가'),
-                  backgroundColor: Colors.black,
+              opacity: isOpend ? 1 : 0,
+              child: Container(
+                width: 131,
+                height: 46,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                )),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFFFFFFF),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      ),
+                    ),
+                  ),
                   onPressed: () {
                     context.go('/createItem');
                   },
+                  child: Center(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Icon(Icons.favorite, size: 14, color: Colors.black),
+                        Text(
+                          '아이템 생성',
+                          style: TextStyle(fontSize: 14, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
-          if (showBtn)
-            AnimatedContainer(
+          ),
+        if (showBtn)
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeIn,
+            margin: isOpend ? const EdgeInsets.only(bottom: 60, right: 0) : EdgeInsets.zero,
+            child: AnimatedOpacity(
               duration: const Duration(milliseconds: 300),
-              curve: Curves.easeIn,
-              margin: isOpend ? const EdgeInsets.only(bottom: 60, right: 0) : EdgeInsets.zero,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 300),
-                opacity: isOpend ? 1 : 0,
-                child: FloatingActionButton.extended(
-                  heroTag: "addList",
-                  label: const Text(
-                    '리스트 추가',
-                    style: TextStyle(),
+              opacity: isOpend ? 1 : 0,
+              child: Container(
+                width: 131,
+                height: 46,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                )),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFFFFFFF),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
                   ),
-                  backgroundColor: Colors.black,
                   onPressed: () {
                     context.go('/createList');
                   },
+                  child: Center(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Icon(Icons.favorite, size: 14, color: Colors.black),
+                        Text(
+                          '리스트 생성',
+                          style: TextStyle(fontSize: 14, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
-          FloatingActionButton(
-            backgroundColor: Colors.black,
-            child: const Icon(Icons.add),
+          ),
+        AnimatedRotation(
+          duration: const Duration(milliseconds: 300),
+          turns: _turns,
+          child: FloatingActionButton(
+            backgroundColor: bgColor,
+            child: Icon(
+              Icons.add,
+              color: iconColor,
+            ),
             onPressed: () {
               if (isOpend) {
                 isOpend = false;
+                bgColor = Colors.black;
+                iconColor = Colors.white;
+                _turns = 0 * pi;
                 Future.delayed(const Duration(milliseconds: 350), () {
                   showBtn = false;
                   setState(() {});
@@ -71,6 +135,9 @@ class _CustomActionButtonState extends State<CustomActionButton> {
               } else {
                 showBtn = true;
                 Future.delayed(const Duration(milliseconds: 100), () {
+                  bgColor = Colors.white;
+                  iconColor = Colors.black;
+                  _turns = 0.02 * 2 * pi;
                   isOpend = true;
                   setState(() {});
                 });
@@ -78,8 +145,8 @@ class _CustomActionButtonState extends State<CustomActionButton> {
               setState(() {});
             },
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
