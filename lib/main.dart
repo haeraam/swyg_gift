d0e87b72ee0915193e4f4a612244aa0e59496dd4
haeraam <nooroongy@dyphi.com>
2022-12-27 20:56:10 +0900

    remove .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:swyg/pages/start_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
   runApp(const StartPage());
 }
