c278f3fcb46bbc3c243bce181ff11e8043a0b4b8
haeraam <nooroongy@dyphi.com>
2022-12-28 13:48:31 +0900

    remove http

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -8,7 +8,7 @@ class Api {
 
   getbestSelect() async {
     // var res = await http.get(Uri.parse('$host/product/bestSelect'));
-    var res = await http.get(Uri.parse('http://52.91.19.229:8080/product/bestSelect'));
+    var res = await http.get(Uri.parse('https://pickproduct.shop/product/bestSelect'));
     List jsonResponse = json.decode(utf8.decode(res.bodyBytes));
     List<Item> items = jsonResponse.map((data) => Item.fromJson(data)).toList();
     return items;
@@ -16,28 +16,28 @@ class Api {
 
   getBestKeyWord()async{
     // var res = await http.get(Uri.parse('$host/category/selectBest'));
-    var res = await http.get(Uri.parse('http://52.91.19.229:8080/category/selectBest'));
+    var res = await http.get(Uri.parse('https://pickproduct.shop/category/selectBest'));
     List jsonResponse = json.decode(utf8.decode(res.bodyBytes));
     List<Category> categorys = jsonResponse.map((data) => Category.fromJson(data)).toList();
     return categorys;
   }
   getWeeklyBestItem()async{
     // var res = await http.get(Uri.parse('$host/category/selectBest'));
-    var res = await http.get(Uri.parse('http://52.91.19.229:8080/likeProductList/selectWeek'));
+    var res = await http.get(Uri.parse('https://pickproduct.shop/likeProductList/selectWeek'));
     List jsonResponse = json.decode(utf8.decode(res.bodyBytes));
     List<Category> categorys = jsonResponse.map((data) => Category.fromJson(data)).toList();
     return categorys;
   }
   getBestMember()async{
     // var res = await http.get(Uri.parse('$host/category/selectBest'));
-    var res = await http.get(Uri.parse('http://52.91.19.229:8080/likeMember/select'));
+    var res = await http.get(Uri.parse('https://pickproduct.shop/likeMember/select'));
     List jsonResponse = json.decode(utf8.decode(res.bodyBytes));
     List<Item> items = jsonResponse.map((data) => Item.fromJson(data)).toList();
     return items;
   }
   getNewItem()async{
     // var res = await http.get(Uri.parse('$host/category/selectBest'));
-    var res = await http.get(Uri.parse('http://52.91.19.229:8080/product/selectList'));
+    var res = await http.get(Uri.parse('https://pickproduct.shop/product/selectList'));
     List jsonResponse = json.decode(utf8.decode(res.bodyBytes));
     List<Item> items = jsonResponse.map((data) => Item.fromJson(data)).toList();
     return items;
